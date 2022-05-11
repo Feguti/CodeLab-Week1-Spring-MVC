666d152617ec951b2d666200ccb8927f98033d9d
Felipe Gutierrez <feguti4@gmail.com>
2022-05-11 20:04:23 -0300

    First steps with thymeleaf, adding 3 ways to request and display the defined atributes

--- src/main/java/br/com/CodeLabIFSP/regescweb/Controllers/HelloController.java
+++ src/main/java/br/com/CodeLabIFSP/regescweb/Controllers/HelloController.java
@@ -1,14 +1,36 @@
 package br.com.CodeLabIFSP.regescweb.Controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 
 public class HelloController {
     
     @GetMapping("/hello")
-    public String hello() {
+    public ModelAndView hello() {
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("nome", "Verdin");
+
+        return mv; 
+    }
+
+    @GetMapping("/hello-model")
+    public String hello(Model model) {
+
+        model.addAttribute("nome", "Kenji");
+
+        return "hello"; 
+    }
+
+    @GetMapping("/hello-servlet")
+    public String hello(HttpServletRequest request) {
+
+        request.setAttribute("nome", "Guti");
 
         return "hello"; // O Spring vai renderizar o arquivo que está em templates/hello.html
     }
